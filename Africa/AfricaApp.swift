c98693a8bb28bd953f58da3dd808ebdae2e213ab
PhoonThetPine <thetpine254@gmail.com>
2023-10-13 10:50:23 +0800

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Phoon Thet Pine on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
